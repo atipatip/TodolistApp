ee328ea716e61eeca4ad53c69696b50f7b64b401
atipatip <91176090+atipatip@users.noreply.github.com>
2022-03-24 23:39:28 +0700

    Add files via upload

---
+++ flutterapi/myapp/serializers.py
@@ -0,0 +1,10 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Todolist
+
+
+
+class TodolistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todolist
+        fields = ('id', 'title', 'detail') # '__all__'
